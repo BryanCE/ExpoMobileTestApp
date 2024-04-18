3de04088959df1bd19a5aaf79e22ec47cfd27e99
Bryan <bryanedman86@gmail.com>
2024-04-18 01:45:01 -0600

    Added fonts and a bunch of stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [".app/**/*.{js, jsx, ts, tsx}", "./components/**/*.{js, jsx, ts, tsx}"],
-  theme: {
-    extend: {},
+content: [".app/**/*.{js, jsx, ts, tsx}", "./components/**/*.{js, jsx, ts, tsx}"],content: ["./app/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+theme: {
+    extend: {
+      colors: {
+        primary: "#161622",
+        secondary: {
+          DEFAULT: "#FF9C01",
+          100: "#FF9001",
+          200: "#FF8E01",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          DEFAULT: "#F2F2F2",
+          100: "#CDCDE0",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextraLight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psembodied: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    }
   },
   plugins: [],
 }
