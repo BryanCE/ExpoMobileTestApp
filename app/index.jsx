a86df4cb0521b97f8e53231b61fb79bb5fa3e6b7
Bryan <bryanedman86@gmail.com>
2024-04-18 00:46:32 -0600

    Trying to do nativewind css

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,13 @@
 //HOME PAGE OF APP, SAME AS / ROUTE
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Bryan's First Mobile</Text>
+    <View className="flex-1 items-center justify-center bg-red" >
+      <Text className="text-3xl" >Bryan's First Mobile</Text>
       <StatusBar style="auto" />
       <Link 
       href="/profile"
@@ -18,12 +18,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
