8496e2bd16e8cc663e1a39ba3fbcd40924d76d71
BryanCE <45415038+BryanCE@users.noreply.github.com>
2024-04-21 00:59:25 -0600

    Feature (#1)
    * Got Home Icon working, my god this is fiddly!
    
    * styling home icon
    
    * Got landing page and CustomButton done.

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,38 +1,110 @@
 //holds all the Tabs for the app
-import { View, Text } from 'react-native'
-import React from 'react'
-import {Tabs, Redirect} from 'expo-router';
-import { icons } from '../../constants';
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs, Redirect } from "expo-router";
+import { icons } from "../../constants";
 
-const TabIcon = ({icon, color, name, focused}) => {
-	return (
-		<View>
-			<Image
-			source={icon} 
-			resizeMode="contain"
-			tintColor={color}
-			className="w-6 h-6"
-			/>
-			<Text className="text-3xl font-pblack "  >{name}</Text>
-		</View>
-	)
-}
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="items-center justify-center">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${
+          focused
+            ? "text-blue-500 font-pbold justify-center"
+            : "text-gray-500 font-pregular justify-center"
+        }`}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
-	<>
-		<Tabs>
-			<Tabs.Screen name="home" 
-			options={{
-				title: 'Home',
-				headerShown: false,
-				tabBarIcon: ({color, focused}) => (
-					<TabIcon icon={icons.home} color={color} name="Home" size={24} focused={focused} />
-				)
-			}}/>
-		</Tabs>
-	</>
-  )
-}
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "gray",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "gray",
+            height: 80,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.animalspiral}
+                color={color}
+                name={"Home"}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                name={"Bookmark"}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                name={"Create"}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name={"Profilex"}
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,46 @@
 //HOME PAGE OF APP, SAME AS / ROUTE
 
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router'; 
-import { styled } from 'nativewind';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, Text, View, Image } from "react-native";
+import { Link, Redirect, router } from "expo-router";
+import { styled } from "nativewind";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 const StyledText = styled(Text);
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white" >
-      <Text className="text-3xl font-pblack " >Bryan's First Mobile</Text>
-      <Text className="text-3xl font-pblack " >App</Text>
-      <StatusBar style="auto" />
-      <Link 
-      href="/home"
-      style={{color: 'blue', marginTop: 20}}
-      >
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full h-full justify-center items-center gap-4">
+          <View className="mb-10">
+            <Text className="text-5xl text-center font-pbold text-white max-w-full ">
+              Welcome to{"\n"}Shooting Utah
+            </Text>
+          </View>
+          <View className="w-80 h-80 rounded-full bg-white flex justify-center items-center">
+            <Image
+              source={images.bronzeboat}
+              resizeMode="contain"
+              className="w-80 h-80"
+            />
+          </View>
+          <View className="relative mt-20 mb-10">
+            <Text className="text-3xl text-white font-pbold">
+              Create an account
+            </Text>
+          </View>
+          <View className="justify-center align-center gap-4 w-full ">
+            <CustomButton
+              title={"Sign Up"}
+              handlePress={() => router.push("/sign-in")}
+              containerStyles="w-full mt-7"
+            />
+          </View>
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
